501693e5bc55a85959bcc9fd2b6c8b5c924d4929
moejehad <mohammedmadhoun18@gmail.com>
2022-06-03 21:49:10 +0300

    DetailsFragment

--- app/src/main/java/com/moejehad/image_search/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/moejehad/image_search/ui/gallery/GalleryViewModel.kt
@@ -1,18 +1,18 @@
 package com.moejehad.image_search.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.moejehad.image_search.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state : SavedStateHandle
 ) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
+
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
     }
@@ -22,6 +22,7 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
+        const val CURRENT_QUERY = "current_query"
         const val DEFAULT_QUERY = "free palestine"
     }
 }
